55490c046a36a9c2ad88d5a5bddc8fe8ccde3cde
Надежда <@-VAIO>
2018-11-18 20:52:03 +0700

    Приложение перестало вылетать, но при нажатии на кнопку "Сохранить", фото карточки так и не появилось.

--- app/src/main/java/goodlne/info/cardbag/Category.java
+++ app/src/main/java/goodlne/info/cardbag/Category.java
@@ -1,6 +1,9 @@
 package goodlne.info.cardbag;
 
-public class Category {
+import android.os.Parcel;
+import android.os.Parcelable;
+
+public class Category implements Parcelable {
     private int id;
     private String name;
 
@@ -9,6 +12,23 @@ public class Category {
         this.id=id;
         this.name=name;
     }
+
+    protected Category(Parcel in) {
+        id = in.readInt();
+        name = in.readString();
+    }
+
+    public static final Creator<Category> CREATOR = new Creator<Category>() {
+        @Override
+        public Category createFromParcel(Parcel in) {
+            return new Category(in);
+        }
+
+        @Override
+        public Category[] newArray(int size) {
+            return new Category[size];
+        }
+    };
 
     public int getId() {
 
@@ -20,4 +40,15 @@ public class Category {
         return name;
     }
 
+    @Override
+    public int describeContents() {
+        return 0;
+    }
+
+    @Override
+    public void writeToParcel(Parcel dest, int flags) {
+        dest.writeInt(id);
+        dest.writeString(name);
+
+    }
 }
